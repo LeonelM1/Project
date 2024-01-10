e3c504b005ca42d5413b7daf02983f0016867342
LeonelM1 <156251698+LeonelM1@users.noreply.github.com>
2024-01-10 16:04:29 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
-
+let mySprite = sprites.create(img`
+    ...............bbbbbbbbbbbbbbbbbbb...............
+    ...........bbbbdd111111111111111ddbbbb...........
+    ........bbbd1111111111111111111111111dbbb........
+    ......bbd11111111dddddddddddddd111111111dbb......
+    ....bbd1111111ddd11111111111111dddd1111111dbb....
+    ...bd111111ddd111111111111111111111ddd111111db...
+    ..bd11111ddd111ddddddddddddddddddd111ddd11111db..
+    .bd11111dd111dddd111111111111111dddd111dd11111db.
+    .b11111d111ddd111111111111111111111ddd111d11111b.
+    bd11111d1ddd1111111111111111111111111ddd1111111db
+    b11111d1ddd111111111111111111111111111ddd1d11111b
+    b11111ddddd111111111111111111111111111ddddd11111b
+    b11111ddddd111111111111111111111111111dddbd11111b
+    b111111dddd111111111111111111111111111dddb111111b
+    bd111111dddd1111111111111111111111111dddbd11111db
+    .b1111111dddd11111111111111111111111dddbd111111b.
+    .bd1111111dbbdd1111111111111111111dddbbd111111db.
+    ..bd11111111dbbdd111111111111111dddbbd1111111db..
+    ...bd111111111dbbbbbbdddddddddddddd111111111db...
+    ....bbd11111111111dbbbbbbbbbddd11111111111dbb....
+    ......bbdd11111111111111111111111111111ddbb......
+    ........bbbdd11111111111111111111111ddbbb........
+    ...........bbbbbddd11111111111dddbbbbb...........
+    ................bbbbbbbbbbbbbbbbb................
+    `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . 2 2 b b b b b . . . . . . . 
+    . 2 b 4 4 4 4 4 4 b . . . . . . 
+    2 2 4 4 4 4 d d 4 4 b . . . . . 
+    2 b 4 4 4 4 4 4 d 4 b . . . . . 
+    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+    2 2 b 4 4 4 4 4 4 4 b e . . . . 
+    . 2 b b b 4 4 4 b b b e . . . . 
+    . . e b b b b b b b e e . . . . 
+    . . . e e b 4 4 b e e e b . . . 
+    . . . . . e e e e e e b d b b . 
+    . . . . . . . . . . . b 1 1 1 b 
+    . . . . . . . . . . . c 1 d d b 
+    . . . . . . . . . . . c 1 b c . 
+    . . . . . . . . . . . . c c . . 
+    `, SpriteKind.Player)
